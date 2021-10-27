07102b4f336f8fcae2f19623552caaaabbc72332
Shinpei Aoki <shimpei.aoki@makuake.co.jp>
2021-10-28 01:01:05 +0900

    入ってなかった……

---
+++ XamarinMacTest/Models/Hoge.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+
+namespace XamarinMacTest.Models
+{
+    [Register("Hoge")]
+    public class Hoge : NSObject
+    {
+        private string _inputString;
+        private int _stringLength;
+
+        public Hoge(string inputString)
+        {
+            InputString = inputString;
+        }
+
+        [Export("InputString")]
+        public string InputString {
+            get { return _inputString; }
+            set
+            {
+                WillChangeValue("InputString");
+                WillChangeValue("StringLength");
+                _inputString = value;
+                DidChangeValue("InputString");
+                DidChangeValue("StringLength");
+            }
+        }
+
+        [Export("StringLength")]
+        public int Price {
+            get { return InputString.Length; }
+        }
+    }
+}
